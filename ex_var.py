b66e728992e047b74f3657db6850fdad5daa3d32
Milad <51044297+miladstar77@users.noreply.github.com>
2019-12-17 10:27:54 +0330

    define mail and database information in this file

---
+++ ex_var.py
@@ -0,0 +1,11 @@
+# define information about email
+mail_pass = "password"
+mail_from = "src-email@domain.ir"
+mail_to = "destination-email@domain.ir"
+mail_server = 'smtm_server_ip_address'
+
+# define information about database
+var_db_host = "localhost"
+var_db_user = "root"
+var_db_password = "password"
+var_db_remote_database = "snort"
